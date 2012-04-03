8ec0eb278175b6f92405e407f574b427353cf30e
Nick Nikolov <nrnickolov@yahoo.com>
2012-04-02 23:38:46 -0400

    Update game

--- app/controllers/games_controller.rb
+++ app/controllers/games_controller.rb
@@ -75,7 +75,7 @@ class GamesController < ApplicationController
 
     respond_to do |format|
       if @game.update_attributes(params[:game])
-        format.html { redirect_to(@game, :notice => 'Game was successfully updated.') }
+        format.html { redirect_to(season_games_path(@game.season_id), :notice => 'Game was successfully updated.') }
         format.xml  { head :ok }
       else
         format.html { render :action => "edit" }
