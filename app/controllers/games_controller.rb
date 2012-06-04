2ef0e1c5fcfef4c762ad26a3a0b0fc3bae0ab699
Nick Nikolov <nnikolov@cbnco.com>
2012-06-04 12:25:58 -0400

    Modify games without javascript

--- app/controllers/games_controller.rb
+++ app/controllers/games_controller.rb
@@ -68,7 +68,7 @@ class GamesController < ApplicationController
 
     respond_to do |format|
       if @game.save
-        format.html { redirect_to(season_games_path(@game.season_id), :notice => 'Game was successfully created.') }
+        format.html { redirect_to(season_game_path(@game.season_id, @game), :notice => 'Game was successfully created.') }
         format.xml  { render :xml => @game, :status => :created, :location => @game }
       else
         format.html { render :action => "new" }
