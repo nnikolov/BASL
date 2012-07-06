4e56f237db015fa5cd35aeece56cb0dd6da2316b
Nick Nikolov <nrnickolov@yahoo.com>
2012-07-06 18:27:32 -0400

    Order games by time and id

--- app/models/season.rb
+++ app/models/season.rb
@@ -1,6 +1,6 @@
 class Season < ActiveRecord::Base
   has_many :teams, :order => "name"
-  has_many :games, :order => "time"
+  has_many :games, :order => "time, id"
 
   def before_save
     if self.current # Set all other seasons to not current
