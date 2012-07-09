a4ee5739464c2c03bb6458b04db5cf600cae23f6
Nick Nikolov <nnikolov@cbnco.com>
2012-07-09 10:23:19 -0400

    Fix after_save

--- app/models/game.rb
+++ app/models/game.rb
@@ -4,8 +4,10 @@ class Game < ActiveRecord::Base
   belongs_to :field
   belongs_to :home_team, :class_name => "Team", :foreign_key => 'home_team_id'
   belongs_to :away_team, :class_name => "Team", :foreign_key => 'away_team_id'
+  after_save :clear_standings
 
-  def after_save
+  # Force the standings to be recalculated by deleting the current standings.
+  def clear_standings
     Standing.delete_all(["season_id = ? and game_type_id = ?", self.season_id, self.game_type_id])
   end
 end
