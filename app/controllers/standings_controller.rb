842a754a1e9fcdbbf2a16da414e0bf6149f8333a
Nick Nikolov <nrnickolov@yahoo.com>
2012-08-24 17:47:51 -0400

    Add xml and json for standings

--- app/controllers/standings_controller.rb
+++ app/controllers/standings_controller.rb
@@ -6,6 +6,14 @@ class StandingsController < ApplicationController
   def index
     @season_standings = Standing.season(:season_id => @season.id)
     @playoff_standings = Standing.playoff(:season_id => @season.id)
+
+    respond_to do |format|
+      format.html # index.html.erb
+      combined = {:season => @season_standings, :playoffs => @playoff_standings}
+      format.xml  { render :xml => combined}
+      format.json  { render :json => combined}
+    end
+
   end
 
 end
