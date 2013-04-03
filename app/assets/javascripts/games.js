c4ac269dd4bed9e17a905579c0da77d42118d777
Nick Nikolov <nrnickolov@yahoo.com>
2013-04-03 15:03:32 -0400

    Narrow down games on team and oponent

--- app/assets/javascripts/games.js
+++ app/assets/javascripts/games.js
@@ -13,23 +13,36 @@ $.each(elementsArray, function(index, value) {
      $('#showTeam')
          .append($("<option></option>")
          .text(value)); 
+     $('#showVsTeam')
+         .append($("<option></option>")
+         .text(value)); 
 });
 //add an event to the select drop down to filter rows out based on team selection
-$("#showTeam").change(function () {
-        var searchFor = $.trim($(this).val());
-//only search for the team name forget about the (red & blue)
-searchFor = searchFor.replace(/(.*)(\(.*)/gi,"$1");
-$("#games tr:gt(0)").hide();
-if(searchFor != "All Teams"){
-$("#games td:contains('" + searchFor + "')").parent().show();
+$("#showVsTeam").change(function (){
+	displayGames();
+})
+$("#showTeam").change(function (){
+	displayGames();
+})
+
+function displayGames() {
+	var searchFor = $.trim($("#showTeam").val());
+	var searchForVs = $.trim($("#showVsTeam").val());
+	//only search for the team name forget about the (red & blue)
+	searchFor = searchFor.replace(/(.*)(\(.*)/gi,"$1");
+	searchForVs = searchForVs.replace(/(.*)(\(.*)/gi,"$1");
+	var patt1 = new RegExp(searchFor);
+	var patt2 = new RegExp(searchForVs);
+	$("#games tr:gt(0)").hide();
+	$("#games tr")
+	.filter(function () {
+	return patt1.test($(this).text()) && patt2.test($(this).text());
+	}).show();
+	
+	////always show the playoff and friendly and final 
+	//$("#games td:contains('Playoff')").parent().show();
+	//$("#games td:contains('Friendly')").parent().show();
+	//$("#games td:contains('Final')").parent().show();
 }
-else
-{
-$("#games tr").show();
-}
-//always show the playoff and friendly and final 
-$("#games td:contains('Playoff')").parent().show();
-$("#games td:contains('Friendly')").parent().show();
-$("#games td:contains('Final')").parent().show();
-    })
+
 });
