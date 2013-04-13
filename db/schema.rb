c18ef4d6cb61dbeec3538e23c3d4ffcb2e1aca02
Nick Nikolov <nrnickolov@yahoo.com>
2013-04-13 15:41:01 -0400

    Clean up

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended to check this file into your version control system.
 
-ActiveRecord::Schema.define(:version => 20130408171907) do
+ActiveRecord::Schema.define(:version => 20130411184254) do
 
   create_table "commercial_listings", :force => true do |t|
     t.string   "company_name"
