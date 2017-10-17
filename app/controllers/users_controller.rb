c816419c498b8368f11163c96ff7e18174897be6
Nick Nikolov <nrnickolov@yahoo.com>
2017-10-17 09:03:44 -0400

    Sort users list

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -6,7 +6,7 @@ class UsersController < ApplicationController
   # GET /users
   # GET /users.xml
   def index
-    @users = User.all
+    @users = User.all.order(admin: :desc, website: :desc, active: :desc, name: :asc)
 
     respond_to do |format|
       format.html # index.html.erb
